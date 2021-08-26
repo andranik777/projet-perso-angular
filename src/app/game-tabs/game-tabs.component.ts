b3e8c5be3a69b83edc74bac87b07ac6c58351de8
andranik777 <andranik777@users.noreply.github.com>
2021-08-26 18:06:44 +0200

    game table update

--- src/app/game-tabs/game-tabs.component.ts
+++ src/app/game-tabs/game-tabs.component.ts
@@ -6,7 +6,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./game-tabs.component.scss']
 })
 export class GameTabsComponent implements OnInit {
+
   @Input() game: any;
+
+  menuList = ["About","Screenshots","Trailers"]
+  selectedList :any = "About";
+
+  openMenulist(menuList:any) {
+    this.selectedList = menuList;
+  }
 
   constructor() { }
 
